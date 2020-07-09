edce3d0c832600ae7adaf6ed00e2a05cca6bd35d
gngfang <63154900+gngfang@users.noreply.github.com>
2020-07-09 18:22:09 -0400

    added the logout if and else statement

--- stock_app/models.py
+++ stock_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Stock(models.Model):
-    ticker = models.CharField(max_length=20)
+# class Stock(models.Model):
+#     ticker = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.ticker
+#     def __str__(self):
+#         return self.ticker
--- stock_app/views.py
+++ stock_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 import requests
 import json
 
@@ -45,4 +48,19 @@ def follow_stock(request):
 
 
 def login_user(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You successfully logged in'))
+            return redirect('home')
+
+        else:
+
+            messages.success(request, ('Error logging in. Please try again'))
+            return redirect('about')
+    else:
+
+        return render(request, 'login.html')
